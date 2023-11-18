5f144f92cd96cbf07e7f9a995cdc20d596b6545f
(っ◔◡◔)っ ♥ Denis ♥ <ctrike163@gmail.com>
2023-11-18 20:17:52 +0300

    Fix: big main1

--- Main1.py
+++ Main1.py
@@ -1,14 +1,14 @@
 def main_1():
     max=0
-    n = 999
-    for i in range(100, n + 1):
-        a_str = str(i)
+    for i in range(100,999+1):
+        a_str =str(i)
         a = int(a_str[0])
         b = int(a_str[1])
         c = int(a_str[2])
         m = i/(a+b+c)
         if max < m:
             max = m
-            print(f'Максимальное значение {m} от числа {i}')
+            s = i
+    print(f'Максимальное значение {max} от числа {s}')
 
 main_1()
