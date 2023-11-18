987ce4f433e5f13a438fdc74c10a4db4508f7c30
(っ◔◡◔)っ ♥ Denis ♥ <ctrike163@gmail.com>
2023-11-18 23:22:31 +0300

    Fix: print in main3

--- Main3.py
+++ Main3.py
@@ -1,7 +1,7 @@
 password = "1234"
 auth = input('Введите пароль: ')
 while password != auth:
-    print('Попробуй еще раз')
+    print('Пароль введен не верно')
     auth = input('Попробуйте еще раз: ')
 else:
     print('Пароль введен верно')
